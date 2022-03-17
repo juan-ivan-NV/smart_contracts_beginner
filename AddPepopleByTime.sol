7cbbbf7e655315555c0de36a78e241049327752b
juan-ivan-NV <coldcram14@hotmail.com>
2022-03-16 21:29:24 -0600

    adding people after a timestamp

---
+++ AddPepopleByTime.sol
@@ -0,0 +1,36 @@
+pragma solidity 0.5.1;
+
+contract MyContract{
+    uint256 public peopleCount = 0;
+    mapping(uint => Person) public people;
+
+    uint256 openingTime = 1647487658;
+
+    modifier onlyWhileOpen() {
+        require(block.timestamp >= openingTime);
+        _;
+    }
+
+    struct Person {
+        uint _id;
+        string _firstName;
+        string _lastName;
+    }
+
+    function addPerson(
+        string memory _firstName, 
+        string memory _lastName
+    ) 
+        public 
+        onlyWhileOpen
+    {
+        incrementCount();
+        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
+
+    function incrementCount() internal {
+        peopleCount += 1;
+    }
+}
+
+/// this function lets you add people pasing a timestamp
