d4f6d5e474faa4962e801b0f81366e59b25795af
juan-ivan-NV <coldcram14@hotmail.com>
2022-03-26 21:55:03 -0600

    import math from sepparate file

--- ImportMath.sol
+++ ImportMath.sol
@@ -1,12 +1,6 @@
 pragma solidity 0.5.1;
 
-library Math {
-    function divide(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b > 0);
-        uint256 c = a / b;
-        return c;
-    }
-}
+import "./Math.sol"; 
 
 contract MyContract {
     uint256 public value;
