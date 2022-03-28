2547d5d98266937971ab16442cad54c429e9c245
juan-ivan-NV <coldcram14@hotmail.com>
2022-03-27 20:09:22 -0600

    testing safe math library

--- ImportMath.sol
+++ ImportMath.sol
@@ -1,13 +1,12 @@
 pragma solidity 0.5.1;
 
-import "./Math.sol"; 
+import "./SafeMath.sol";
 
 contract MyContract {
+    using SafeMath for uint256;
     uint256 public value;
 
     function calculate(uint _value1, uint _value2) public {
-        value = Math.divide(_value1, _value2);
+        value = _value1.div(_value2);
     }
 }
-
-/// import Math function from a library
